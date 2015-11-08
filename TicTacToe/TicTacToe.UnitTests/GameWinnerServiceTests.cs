d12a620a5a96802d3c274426dc83f420888f4830
Jeff McKenzie <mail@mcknz.com>
2015-11-07 22:06:17 -0500

    added first test

--- TicTacToe/TicTacToe.UnitTests/GameWinnerServiceTests.cs
+++ TicTacToe/TicTacToe.UnitTests/GameWinnerServiceTests.cs
@@ -10,6 +10,18 @@ namespace TicTacToe.UnitTests
     [TestFixture]
     public class GameWinnerServiceTests
     {
+        [Test]
+        public void NeitherPlayerHasThreeInARow()
+        {
+            const char expected = ' ';
+            var gameBoard = new char[3, 3] {
+                {' ',' ',' '},
+                {' ',' ',' '},
+                {' ',' ',' '}
+            };
+            var actual = gameWinnerService.Validate(gameBoard);
+            Assert.AreEqual(expected, actual);
+        }
 
     }
 }
